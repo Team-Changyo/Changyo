b4a736b248006cdf64e908edce6cc06ca6213295
wjs_5025 <wjs_5025@naver.com>
2023-09-03 02:56:28 +0900

    refactor: render 방식 개선

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,5 @@
-import AppLayout from 'layouts/common/AppLayout';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RecoilRoot } from 'recoil';
 import AppRouter from 'router/AppRouter';
-import { GlobalStyles } from 'styles/GlobalStyles';
 
-const Render = (
-	<AppLayout>
-		<RecoilRoot>
-			<GlobalStyles />
-			<AppRouter />
-		</RecoilRoot>
-	</AppLayout>
-);
-
-ReactDOM.createRoot(document.getElementById('root')!).render(Render);
+ReactDOM.createRoot(document.getElementById('root')!).render(<AppRouter />);
--- client/src/router/AppRouter.tsx
+++ client/src/router/AppRouter.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AccountPage from 'pages/AccountPage';
+import Tabbar from 'components/organisms/common/Tabbar';
+import AppLayout from 'layouts/common/AppLayout';
+import { GlobalStyles } from 'styles/GlobalStyles';
+import { RecoilRoot } from 'recoil';
 
 function AppRouter() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<AccountPage />} />
-			</Routes>
-		</BrowserRouter>
+		<RecoilRoot>
+			<GlobalStyles />
+			<BrowserRouter>
+				<AppLayout>
+					<Routes>
+						<Route path="/" element={<AccountPage />} />
+						<Route path="/qr" element={<AccountPage />} />
+						<Route path="/deposit" element={<AccountPage />} />
+					</Routes>
+					<Tabbar />
+				</AppLayout>
+			</BrowserRouter>
+		</RecoilRoot>
 	);
 }
 
