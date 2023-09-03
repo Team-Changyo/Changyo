64fb3de3fc298f546ec9ced43b5104150d4f749e
Chaos0103 <lyt1228@naver.com>
2023-09-03 20:06:22 +0900

    feat: 공통 API Response 구현

---
+++ server/src/main/java/com/shinhan/changyo/api/ApiResponse.java
@@ -0,0 +1,31 @@
+package com.shinhan.changyo.api;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@Getter
+public class ApiResponse<T> {
+
+    private final int code;
+    private final HttpStatus status;
+    private final String message;
+    private final T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
+        return new ApiResponse<>(status, message, data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return of(OK, "SUCCESS", data);
+    }
+
+}
