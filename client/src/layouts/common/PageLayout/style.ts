6a800005ecc7b12a07fc0893979eaef81fa33dd5
wjs_5025 <wjs_5025@naver.com>
2023-09-03 16:44:47 +0900

    design: layout 마진&패딩 추가

--- client/src/layouts/common/PageLayout/style.ts
+++ client/src/layouts/common/PageLayout/style.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const PageLayoutContainer = styled.div`
 	position: relative;
+	max-width: 500px;
 	min-height: 100vh;
 	max-height: 100vh;
 	overflow-y: scroll;
+	padding: 0 10px;
 `;
