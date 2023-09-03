7b79fc81749ba0202c8a3fb0c69574564503c5c0
wjs_5025 <wjs_5025@naver.com>
2023-09-03 16:55:26 +0900

    feat: AccountPage 컴포넌트 구현

---
+++ client/src/layouts/page/AccountPageLayout/style.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const AccountPageLayoutContainer = styled.div`
+	position: relative;
+	margin-top: 44px;
+
+	.navigation {
+		position: fixed;
+		width: 100%;
+	}
+`;
