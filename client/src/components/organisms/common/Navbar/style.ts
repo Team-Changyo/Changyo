f9cf3a9a0d9cd8680b0764a9c88d92bbcb6cd837
wjs_5025 <wjs_5025@naver.com>
2023-09-03 16:43:13 +0900

    feat: navbar 컴포넌트 구현

---
+++ client/src/components/organisms/common/Navbar/style.ts
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+export const NavbarWrapper = styled.aside`
+	position: fixed;
+	right: 0;
+	left: 0;
+	top: 0;
+	z-index: 20;
+`;
+
+export const NavbarContainer = styled.nav`
+	display: flex;
+	align-items: center;
+	width: 100%;
+	max-width: 500px;
+	height: 44px;
+	margin: 0 auto;
+`;
