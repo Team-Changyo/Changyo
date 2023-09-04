4759bc7072102de1f0021f23aab4f0ea22aa8c4c
wjs_5025 <wjs_5025@naver.com>
2023-09-05 00:47:08 +0900

    design: setting color set

--- client/src/styles/GlobalStyles.ts
+++ client/src/styles/GlobalStyles.ts
@@ -9,35 +9,26 @@ export const GlobalStyles = createGlobalStyle`
         --main-color : #2449FF;
         --sub-color : #6B6B6F;
 
-        --gray-500 : #6B6B6F;
-        --gray-400 : #BEBEC4;
-        --gray-300 : #EDEDEF;
-        --gray-200 : #F8F8FA;
-        --gray-100 : #f2f2f2;
+        --gray-500 : #515457;
+        --gray-400 : #6C6E70;
+        --gray-300 : #ACACAD;
+        --gray-100 : #F3F3F3;
 
         --black-500 : #000000;
-        --black-400 : #4B4B4B;
         --black-300 : #3B3B3B;
-        --black-200 : #2B2B2B;
-        --black-100 : #1B1B1B;
+        --black-100 : #2A2A2A;
+
+        /* radius */
+        --radius-s : 5px;
+        --radius-m : 10px;
+        --radius-l : 99px;
     }
 
     body{
+        font-family: 'Pretendard';
         padding: 0;
         margin: 0;
-        font-family: 'Noto Sans KR', sans-serif;
     };
-    button{
-        display: flex;
-        cursor: pointer;
-        outline: none;
-        border-radius: 3px;
-    };
-    input{
-        display: flex;
-        outline: none;
-        padding-left: 10px;
-    }
 
     // 스크롤 바
     ::-webkit-scrollbar {
