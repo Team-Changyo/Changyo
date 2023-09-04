36bae3f30278739c6c920634b1a8472f02055b94
wjs_5025 <wjs_5025@naver.com>
2023-09-05 00:52:09 +0900

    feat: AccountSummary 컴포넌트 (계좌 총 개수, 토탈 금액)

--- client/src/components/organisms/account/AccountSummary/index.tsx
+++ client/src/components/organisms/account/AccountSummary/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import SmallSubText from 'components/atoms/common/SmallSubText';
+import LargeMoneyText from 'components/atoms/common/LargeMoneyText';
 import { AccountSummaryContainer } from './style';
 
-function AccountSummary() {
-	return <AccountSummaryContainer>총 1개 ㅇㅇㅇ원</AccountSummaryContainer>;
+/**
+ * 계좌 총 개수, 토탈 금액 표기
+ */
+function AccountSummary({ accountCnt, totalMoney }: { accountCnt: number; totalMoney: number }) {
+	return (
+		<AccountSummaryContainer>
+			<SmallSubText text={`총 ${accountCnt}개의 계좌`} />
+			<LargeMoneyText money={totalMoney} />
+		</AccountSummaryContainer>
+	);
 }
 
 export default AccountSummary;
--- client/src/components/organisms/account/AccountSummary/style.ts
+++ client/src/components/organisms/account/AccountSummary/style.ts
@@ -1,3 +1,6 @@
 import { styled } from 'styled-components';
 
-export const AccountSummaryContainer = styled.div``;
+export const AccountSummaryContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
