a77fc898a36c2187d21c14b87f5e0a493965827e
Chaos0103 <lyt1228@naver.com>
2023-09-05 07:32:45 +0900

    feat: 생성자 및 빌더패턴 적용

--- api/src/main/java/com/shinhan/api/api/controller/account/request/AccountRequest.java
+++ api/src/main/java/com/shinhan/api/api/controller/account/request/AccountRequest.java
@@ -1,9 +1,17 @@
 package com.shinhan.api.api.controller.account.request;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class AccountRequest {
 
     private String accountNumber;
+
+    @Builder
+    public AccountRequest(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
 }
--- api/src/main/java/com/shinhan/api/api/controller/account/response/AccountResponse.java
+++ api/src/main/java/com/shinhan/api/api/controller/account/response/AccountResponse.java
@@ -1,5 +1,6 @@
 package com.shinhan.api.api.controller.account.response;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -8,6 +9,7 @@ public class AccountResponse {
     private String accountNumber;
     private int balance;
 
+    @Builder
     public AccountResponse(String accountNumber, int balance) {
         this.accountNumber = accountNumber;
         this.balance = balance;
