092220b3d4431d82a80c71a447f16098bf6c746c
wjs_5025 <wjs_5025@naver.com>
2023-09-07 03:23:27 +0900

    refactor: Tabbar 렌더조건 추가

--- client/src/components/organisms/common/Tabbar/index.tsx
+++ client/src/components/organisms/common/Tabbar/index.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { TabbarItem, TabbarCenterItem } from 'components/atoms/common/TabbarItem';
+import { useLocation } from 'react-router-dom';
+import { TABBAR_RENDER_EXCEPTS } from 'constants/tabbarRenderExcepts';
 import { TabBackground, TabContainer, TabWrapper } from './style';
 
 function Tabbar() {
-	return (
-		<TabWrapper>
-			<TabContainer>
-				<TabbarItem pathname="/qr" />
-				<TabbarCenterItem />
-				<TabbarItem pathname="/deposit" />
-				<TabBackground>
-					<div id="box" />
-				</TabBackground>
-			</TabContainer>
-		</TabWrapper>
-	);
+	const location = useLocation();
+	const [isRender, setIsRender] = useState(true);
+
+	useEffect(() => {
+		if (TABBAR_RENDER_EXCEPTS.includes(location.pathname)) {
+			setIsRender(false);
+		} else {
+			setIsRender(true);
+		}
+	}, [location, isRender]);
+
+	if (isRender)
+		return (
+			<TabWrapper>
+				<TabContainer>
+					<TabbarItem pathname="/qr" />
+					<TabbarCenterItem />
+					<TabbarItem pathname="/deposit" />
+					<TabBackground>
+						<div id="box" />
+					</TabBackground>
+				</TabContainer>
+			</TabWrapper>
+		);
 }
 
 export default Tabbar;
---
+++ client/src/constants/tabbarRenderExcepts.ts
@@ -0,0 +1,1 @@
+export const TABBAR_RENDER_EXCEPTS = ['/qr/create'];
