0f6971056bd87518d1ddae891868273621ac9a39
wjs_5025 <wjs_5025@naver.com>
2023-09-07 00:24:44 +0900

    feat: SubTabNavbar

--- client/src/components/organisms/common/SubTabNavbar/index.tsx
+++ client/src/components/organisms/common/SubTabNavbar/index.tsx
@@ -1,14 +1,29 @@
-import NavbarLayout from 'layouts/common/NavbarLayout';
 import React from 'react';
+import { ReactComponent as Left } from 'assets/icons/navigation/left.svg';
+import NavbarLayout from 'layouts/common/NavbarLayout';
+import { useNavigate } from 'react-router-dom';
 import { SubTabNavbarContainer } from './style';
 
+interface ISubTabNavbarProps {
+	text: string;
+}
 /**
  * 뒤로가기 버튼 + 중앙타이틀이 적혀있는 상단 네비게이션바
  */
-function SubTabNavbar() {
+function SubTabNavbar({ text }: ISubTabNavbarProps) {
+	const navigate = useNavigate();
+
+	const handleLeftClick = () => {
+		navigate(-1);
+	};
 	return (
 		<NavbarLayout>
-			<SubTabNavbarContainer>sub</SubTabNavbarContainer>
+			<SubTabNavbarContainer>
+				<button type="button" className="back-btn">
+					<Left onClick={handleLeftClick} />
+				</button>
+				<h1>{text}</h1>
+			</SubTabNavbarContainer>
 		</NavbarLayout>
 	);
 }
--- client/src/components/organisms/common/SubTabNavbar/style.ts
+++ client/src/components/organisms/common/SubTabNavbar/style.ts
@@ -2,9 +2,25 @@ import { styled } from 'styled-components';
 
 export const SubTabNavbarContainer = styled.div`
 	padding: 0 15px;
+	width: 100%;
+	position: relative;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+
+	.back-btn {
+		position: absolute;
+		left: 5px; // TODO : 임시
+
+		svg {
+			width: 35px;
+			height: 35px;
+		}
+	}
 
 	h1 {
-		font-size: 1.5rem;
+		font-size: 1.1rem;
 		font-weight: bold;
 	}
 `;
