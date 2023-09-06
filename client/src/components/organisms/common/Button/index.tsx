ccc8f721eda4cfab1c90c7d8e4accdfd132e98d2
wjs_5025 <wjs_5025@naver.com>
2023-09-07 00:24:32 +0900

    feat: add icon into Button

--- client/src/components/organisms/common/Button/index.tsx
+++ client/src/components/organisms/common/Button/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { ButtonWrapper } from './style';
 
 interface IButtonProps {
 	handleClick: () => void;
 	text: string;
 	type: 'Normal' | 'Primary' | 'Danger';
+	icon?: ReactNode;
 }
-function Button({ handleClick, text, type }: IButtonProps) {
+
+function Button({ handleClick, text, type, icon }: IButtonProps) {
 	return (
-		<ButtonWrapper $type={type}>
-			<button type="button" onClick={handleClick}>
-				{text}
-			</button>
+		<ButtonWrapper $type={type} onClick={handleClick}>
+			<div className="icon">{icon}</div>
+			<button type="button">{text}</button>
 		</ButtonWrapper>
 	);
 }
