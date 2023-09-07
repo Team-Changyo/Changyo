d58251b00e4cbf04fa335b789485f0596ece8cf9
longBright <longbright0804@gmail.com>
2023-09-07 21:40:25 +0900

    feat: Member Service 생성

---
+++ server/src/main/java/com/shinhan/changyo/api/member/service/MemberService.java
@@ -0,0 +1,19 @@
+package com.shinhan.changyo.api.member.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 계정 관련 서비스
+ * 
+ * @author 최영환
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class MemberService {
+}
