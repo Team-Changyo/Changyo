ef4d32bbd6fab8e070dcf75b3ad3253279afe430
longBright <longbright0804@gmail.com>
2023-09-07 23:13:47 +0900

    feat: 전화번호 중복 체크 쿼리 구현 - MemberQueryRepository#existPhoneNumber

--- server/src/main/java/com/shinhan/changyo/domain/member/repository/MemberQueryRepository.java
+++ server/src/main/java/com/shinhan/changyo/domain/member/repository/MemberQueryRepository.java
@@ -1,7 +1,6 @@
 package com.shinhan.changyo.domain.member.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.shinhan.changyo.domain.member.QMember;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -33,6 +32,22 @@ public class MemberQueryRepository {
                 .selectOne()
                 .from(member)
                 .where(member.loginId.eq(loginId))
+                .fetchFirst();
+
+        return result != null;
+    }
+
+    /**
+     * 전화번호 중복 체크
+     *
+     * @param phoneNumber 중복 체크 할 대상 전화번호
+     * @return 존재하면 true, 존재하지 않으면 false
+     */
+    public boolean existsPhoneNumber(String phoneNumber) {
+        Integer result = queryFactory
+                .selectOne()
+                .from(member)
+                .where(member.phoneNumber.eq(phoneNumber))
                 .fetchFirst();
 
         return result != null;
--- server/src/main/java/com/shinhan/changyo/domain/member/repository/MemberRepository.java
+++ server/src/main/java/com/shinhan/changyo/domain/member/repository/MemberRepository.java
@@ -1,5 +1,6 @@
-package com.shinhan.changyo.domain.member;
+package com.shinhan.changyo.domain.member.repository;
 
+import com.shinhan.changyo.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
