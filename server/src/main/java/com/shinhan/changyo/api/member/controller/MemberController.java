28614bfe9956003d2ab4b4843d7fbbdd2ab0fe76
longBright <longbright0804@gmail.com>
2023-09-07 21:40:35 +0900

    feat: Member Controller 생성

---
+++ server/src/main/java/com/shinhan/changyo/api/member/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.shinhan.changyo.api.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 계정 관련 API 컨트롤러
+ * 
+ * @author 최영환
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class MemberController {
+}
