fb2bf620a10e22b2d79ec58b5ffdcfeeb21ab996
longBright <longbright0804@gmail.com>
2023-09-07 21:22:12 +0900

    feat: configuration SpringSecurity

---
+++ server/src/main/java/com/shinhan/changyo/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.shinhan.changyo.config;
+
+import com.querydsl.core.annotations.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Web Security Config
+ * @author 최영환
+ */
+@Config
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers("/**").permitAll()
+                    .anyRequest().permitAll()
+                    .and()
+                .cors().disable()
+                .csrf().disable()
+                .logout().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
