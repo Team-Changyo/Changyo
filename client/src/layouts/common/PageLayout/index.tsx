332d2f81318a8df16115be949d0fb5c1163ce3ce
wjs_5025 <wjs_5025@naver.com>
2023-09-09 00:27:16 +0900

    refactor: 페이지 레이아웃 개선(탭바 렌더 여부에 따라 PaddingBottom 추가)

--- client/src/layouts/common/PageLayout/index.tsx
+++ client/src/layouts/common/PageLayout/index.tsx
@@ -1,8 +1,18 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
+import useTabbarRender from 'hooks/useTabbarRender';
 import { PageLayoutContainer } from './style';
 
 function PageLayout({ children }: { children: ReactNode }) {
-	return <PageLayoutContainer>{children}</PageLayoutContainer>;
+	const isRender = useTabbarRender();
+	const [paddingBottom, setPaddingBottom] = useState('0px');
+
+	/* 탭바 Render 여부에 따라,  */
+	useEffect(() => {
+		if (isRender) setPaddingBottom('56px');
+		else setPaddingBottom('0px');
+	}, [isRender]);
+
+	return <PageLayoutContainer $paddingBottom={paddingBottom}>{children}</PageLayoutContainer>;
 }
 
 export default PageLayout;
--- client/src/layouts/common/PageLayout/style.ts
+++ client/src/layouts/common/PageLayout/style.ts
@@ -1,10 +1,11 @@
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
-export const PageLayoutContainer = styled.div`
+export const PageLayoutContainer = styled.div<{ $paddingBottom: string }>`
 	position: relative;
 	max-width: 500px;
 	min-height: 100vh;
 	max-height: 100vh;
 	overflow-y: scroll;
 	padding: 0 15px;
+	padding-bottom: ${({ $paddingBottom }) => $paddingBottom};
 `;
