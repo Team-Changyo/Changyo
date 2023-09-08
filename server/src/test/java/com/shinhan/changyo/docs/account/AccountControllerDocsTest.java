fd6a72ab890474bd5f549bd890da445b8ce58dc1
longBright <longbright0804@gmail.com>
2023-09-09 00:57:41 +0900

    feat: 계좌 전체 조회 API Docs 작성

--- server/src/test/java/com/shinhan/changyo/docs/account/AccountControllerDocsTest.java
+++ server/src/test/java/com/shinhan/changyo/docs/account/AccountControllerDocsTest.java
@@ -2,6 +2,9 @@ package com.shinhan.changyo.docs.account;
 
 import com.shinhan.changyo.api.controller.account.AccountController;
 import com.shinhan.changyo.api.controller.account.request.CreateAccountRequest;
+import com.shinhan.changyo.api.controller.account.response.AccountDetailResponse;
+import com.shinhan.changyo.api.controller.account.response.AccountResponse;
+import com.shinhan.changyo.api.service.account.AccountQueryService;
 import com.shinhan.changyo.api.service.account.AccountService;
 import com.shinhan.changyo.api.service.account.dto.CreateAccountDto;
 import com.shinhan.changyo.docs.RestDocsSupport;
@@ -10,12 +13,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,10 +32,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AccountControllerDocsTest extends RestDocsSupport {
 
     private final AccountService accountService = mock(AccountService.class);
+    private final AccountQueryService accountQueryService = mock(AccountQueryService.class);
 
     @Override
     protected Object initController() {
-        return new AccountController(accountService);
+        return new AccountController(accountService, accountQueryService);
     }
 
     @DisplayName("계좌 등록 API")
@@ -82,6 +92,72 @@ public class AccountControllerDocsTest extends RestDocsSupport {
                                         .description("메시지"),
                                 fieldWithPath("data").type(JsonFieldType.NUMBER)
                                         .description("계좌 식별키")
+                        )
+                ));
+    }
+
+    @DisplayName("계좌 전체 조회 API")
+    @Test
+    void getAccounts() throws Exception {
+        Long memberId = 1L;
+
+        AccountDetailResponse account1 = AccountDetailResponse.builder()
+                .accountNumber("110184999999")
+                .balance(200501)
+                .bankCode("088")
+                .mainAccount(true)
+                .build();
+
+        AccountDetailResponse account2 = AccountDetailResponse.builder()
+                .accountNumber("110185999999")
+                .balance(0)
+                .bankCode("088")
+                .mainAccount(false)
+                .build();
+
+        List<AccountDetailResponse> accounts = List.of(account1, account2);
+
+        AccountResponse response = AccountResponse.builder()
+                .totalAccounts(accounts.size())
+                .accountDetailResponses(accounts)
+                .build();
+
+        given(accountQueryService.getAccounts(anyLong()))
+                .willReturn(response);
+
+        mockMvc.perform(
+                        get("/account")
+                                .param("memberId", String.valueOf(memberId))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("search-account",
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("memberId")
+                                        .description("회원 식별키")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.totalAccounts").type(JsonFieldType.NUMBER)
+                                        .description("전체 계좌 개수"),
+                                fieldWithPath("data.accountDetailResponses").type(JsonFieldType.ARRAY)
+                                        .description("계좌 정보 데이터"),
+                                fieldWithPath("data.accountDetailResponses[].accountNumber").type(JsonFieldType.STRING)
+                                        .description("계좌번호"),
+                                fieldWithPath("data.accountDetailResponses[].balance").type(JsonFieldType.NUMBER)
+                                        .description("잔액"),
+                                fieldWithPath("data.accountDetailResponses[].bankCode").type(JsonFieldType.STRING)
+                                        .description("은행코드"),
+                                fieldWithPath("data.accountDetailResponses[].mainAccount").type(JsonFieldType.BOOLEAN)
+                                        .description("주계좌여부")
                         )
                 ));
     }
