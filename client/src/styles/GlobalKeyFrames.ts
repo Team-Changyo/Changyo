ffcc7e9e18666d490fc1d4e01dd4b17032be9737
wjs_5025 <wjs_5025@naver.com>
2023-09-10 00:45:41 +0900

    design: add keyframe

---
+++ client/src/styles/GlobalKeyFrames.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalKeyFrames = createGlobalStyle`
+    @keyframes risingLogo {
+        from {
+            margin-bottom: 0px;
+        }
+        to {
+            margin-bottom: 30rem;
+        }
+    }
+`;
