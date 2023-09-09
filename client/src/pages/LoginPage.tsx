77764ba9c3d241e20ab5ab80f9fcdf1fc87b0bc2
wjs_5025 <wjs_5025@naver.com>
2023-09-10 00:47:35 +0900

    feat: 로그인 페이지 구현

---
+++ client/src/pages/LoginPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PageLayout from 'layouts/common/PageLayout';
+import { ReactComponent as ColorChangyoLogo } from 'assets/imgs/ColorChangyoLogo.svg';
+import { ReactComponent as ShinhanBankLogo } from 'assets/imgs/ShinhanBankLogo.svg';
+import LoginPageLayout from 'layouts/page/LoginPageLayout';
+import LoginForm from 'components/organisms/auth/LoginForm';
+
+function LoginPage() {
+	return (
+		<PageLayout>
+			<LoginPageLayout
+				ChangyoLogo={<ColorChangyoLogo />}
+				LoginForm={<LoginForm />}
+				ShinhanBankLogo={<ShinhanBankLogo />}
+			/>
+		</PageLayout>
+	);
+}
+
+export default LoginPage;
