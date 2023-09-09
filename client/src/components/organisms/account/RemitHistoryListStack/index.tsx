1fb670c93dda77a9df52cdef009bc298b5ab32be
wjs_5025 <wjs_5025@naver.com>
2023-09-10 01:52:19 +0900

    refactor: 불필요한 타입 속성 제거

--- client/src/components/organisms/account/RemitHistoryListStack/index.tsx
+++ client/src/components/organisms/account/RemitHistoryListStack/index.tsx
@@ -7,15 +7,15 @@ function RemitHistoryListStack() {
 		{
 			date: '2023-09-04',
 			history: [
-				{ key: 0, title: '아이센스PC방', time: '23:23', type: 'deposit', price: -11000, balance: 200501 },
-				{ key: 1, title: '아이센스PC방', time: '23:23', type: 'deposit', price: -11000, balance: 200501 },
+				{ key: 0, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
+				{ key: 1, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
 			],
 		},
 		{
 			date: '2023-09-05',
 			history: [
-				{ key: 2, title: '아이센스PC방', time: '23:23', type: 'deposit', price: -11000, balance: 200501 },
-				{ key: 3, title: '아이센스PC방', time: '23:23', type: 'deposit', price: -11000, balance: 200501 },
+				{ key: 2, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
+				{ key: 3, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
 			],
 		},
 	];
