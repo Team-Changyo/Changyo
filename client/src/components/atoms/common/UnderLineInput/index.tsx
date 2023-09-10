e6a2c15a9755b6d4fe184f9d15af6dfdf79c0fa1
wjs_5025 <wjs_5025@naver.com>
2023-09-10 20:48:32 +0900

    refactor: 공통컴포넌트 사용성 개선(타입, 값 등 추가)

--- client/src/components/atoms/common/UnderLineInput/index.tsx
+++ client/src/components/atoms/common/UnderLineInput/index.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { ChangeEvent, Dispatch, SetStateAction, useState } from 'react';
 import Input from '@mui/material/Input';
 import { UnderLineInputWrapper } from './style';
 
-interface IUnderLineInputProps {
+interface IUnderLineInputProps<T> {
+	type: string;
+	value: T;
+	setValue: Dispatch<SetStateAction<T>>;
 	placeholder: string;
 	width: string;
 	unitText?: string;
 }
-function UnderLineInput({ placeholder, unitText, width }: IUnderLineInputProps) {
+function UnderLineInput<T>(props: IUnderLineInputProps<T>) {
+	const { type, value, setValue, placeholder, unitText, width } = props;
+	const [displayValue, setDisplayValue] = useState(value);
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		setValue(e.target.value as T);
+		setDisplayValue(e.target.value as T);
+	};
+
+	console.log(type);
 	return (
 		<UnderLineInputWrapper $width={width}>
-			<Input placeholder={placeholder} /> {unitText}
+			<Input type="" value={displayValue} onChange={handleChange} placeholder={placeholder} /> {unitText}
 		</UnderLineInputWrapper>
 	);
 }
