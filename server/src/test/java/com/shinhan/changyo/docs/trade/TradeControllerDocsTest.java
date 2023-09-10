fa07440d30509265e103cc14c1279b813e37b92a
longBright <longbright0804@gmail.com>
2023-09-10 18:35:37 +0900

    docs: 보증금 송금 API Docs 수정 - TradeControllerDocsTest#createTrade

--- server/src/test/java/com/shinhan/changyo/docs/trade/TradeControllerDocsTest.java
+++ server/src/test/java/com/shinhan/changyo/docs/trade/TradeControllerDocsTest.java
@@ -46,9 +46,10 @@ public class TradeControllerDocsTest extends RestDocsSupport {
                 .accountId(1L)
                 .withdrawalAccountNumber("1102008999999")
                 .qrCodeId(1L)
+                .qrCodeTitle("럭셔리 글램핑 객실이용")
                 .depositAccountNumber("110054999999")
                 .amount(20000)
-                .content("럭셔리 글램핑 객실이용")
+                .content("최영환")
                 .build();
         Long tradeId = 1L;
 
@@ -69,15 +70,17 @@ public class TradeControllerDocsTest extends RestDocsSupport {
                                 fieldWithPath("accountId").type(JsonFieldType.NUMBER)
                                         .description("출금 계좌 식별키"),
                                 fieldWithPath("withdrawalAccountNumber").type(JsonFieldType.STRING)
-                                                .description("출금 계좌 번호"),
+                                        .description("출금 계좌 번호"),
                                 fieldWithPath("qrCodeId").type(JsonFieldType.NUMBER)
                                         .description("보증금 QR 코드 식별키"),
+                                fieldWithPath("qrCodeTitle").type(JsonFieldType.STRING)
+                                        .description("보증금 QR 코드 이름"),
                                 fieldWithPath("depositAccountNumber").type(JsonFieldType.STRING)
                                         .description("입금 계좌 번호"),
                                 fieldWithPath("amount").type(JsonFieldType.NUMBER)
                                         .description("금액"),
                                 fieldWithPath("content").type(JsonFieldType.STRING)
-                                        .description("출금 계좌명")
+                                        .description("출금 계좌 회원 이름")
                         ),
                         responseFields(
                                 fieldWithPath("code").type(JsonFieldType.NUMBER)
