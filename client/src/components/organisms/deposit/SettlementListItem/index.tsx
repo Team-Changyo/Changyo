df64d2e5cca77d0623b4f14e9dcdfa6cb50eb788
wjs_5025 <wjs_5025@naver.com>
2023-09-10 20:50:34 +0900

    feat: SettlementList, SettlementListItem

--- client/src/components/organisms/deposit/SettlementListItem/index.tsx
+++ client/src/components/organisms/deposit/SettlementListItem/index.tsx
@@ -1,45 +1,32 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React from 'react';
 import { ReactComponent as Check } from 'assets/icons/check.svg';
 import { ReactComponent as Coin } from 'assets/icons/account/coin.svg';
-import { ISettlement, ISettlementGroup } from 'types/deposit';
+import { ISettlement } from 'types/deposit';
 import { SettlementListItemContainer } from './style';
-import DepositReturnModal from '../DepositReturnModal';
 
 interface ISettlementListItemProps {
+	addToBeReturned: (settlement: ISettlement) => void;
+	isMultiReturnMode: boolean;
+	openReturnModal: () => void;
 	settlement: ISettlement;
-	multiReturnMode: boolean;
-	selectedSettlement: Array<ISettlement>;
-	setSelectedSettlement: Dispatch<SetStateAction<ISettlement[]>>;
-	settlementGroup: ISettlementGroup;
+	toBeReturned: Array<ISettlement>;
 }
 
-function SettlementListItem({
-	settlement,
-	multiReturnMode,
-	selectedSettlement,
-	setSelectedSettlement,
-	settlementGroup,
-}: ISettlementListItemProps) {
+function SettlementListItem(props: ISettlementListItemProps) {
+	const { addToBeReturned, isMultiReturnMode, openReturnModal, settlement, toBeReturned } = props;
+
+	const handleReturn = () => {
+		toBeReturned.length = 0;
+		addToBeReturned(settlement);
+		openReturnModal();
+	};
+
 	const isSelected = () => {
-		const idx = selectedSettlement.findIndex((el) => el.key === settlement.key);
+		const idx = toBeReturned.findIndex((el) => el.key === settlement.key);
 		if (idx === -1) return false;
 		return true;
 	};
 
-	const handleReturnBtnClick = () => {
-		DepositReturnModal({ depositorName: settlement.depositorName, onAction: () => {}, params: { settlementGroup } });
-	};
-
-	const handleCheckBtnClick = () => {
-		const idx = selectedSettlement.findIndex((el) => el.key === settlement.key);
-		if (idx === -1) {
-			selectedSettlement.push(settlement);
-			setSelectedSettlement([...selectedSettlement]);
-		} else {
-			selectedSettlement.splice(idx, 1);
-			setSelectedSettlement([...selectedSettlement]);
-		}
-	};
 	return (
 		<SettlementListItemContainer $isReturned={settlement.isReturned}>
 			<div className="settlement-logo">
@@ -62,12 +49,15 @@ function SettlementListItem({
 				) : (
 					// 반환되지 않은 건이면, 반환 버튼 or 체크 버튼
 					<>
-						{multiReturnMode ? (
+						{isMultiReturnMode ? (
 							// 여러건 반환 모드일 때, 체크 버튼으로 전환
-							<Check fill={isSelected() ? 'var(--main-color)' : 'var(--gray-400)'} onClick={handleCheckBtnClick} />
+							<Check
+								fill={isSelected() ? 'var(--main-color)' : 'var(--gray-400)'}
+								onClick={() => addToBeReturned(settlement)}
+							/>
 						) : (
 							// 단일건 반환 모드일 때, 반환 버튼으로 전환
-							<button type="button" className="before-return" onClick={handleReturnBtnClick}>
+							<button type="button" className="before-return" onClick={handleReturn}>
 								반환
 							</button>
 						)}
