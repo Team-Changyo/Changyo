69dbbc638b3b16930a229c16cac419a256f28f14
longBright <longbright0804@gmail.com>
2023-09-10 18:18:23 +0900

    feat: 보증금 송금 API 요청 객체 수정 - CreateTradeRequest.java - CreateTradeDto.java

--- server/src/main/java/com/shinhan/changyo/api/controller/trade/request/CreateTradeRequest.java
+++ server/src/main/java/com/shinhan/changyo/api/controller/trade/request/CreateTradeRequest.java
@@ -14,16 +14,22 @@ public class CreateTradeRequest {
 
     @NotNull
     private Long accountId;
+    @NotBlank
+    private String withdrawalAccountNumber;
     @NotNull
     private Long qrCodeId;
+    @NotBlank
+    private String depositAccountNumber;
     private int amount;
     @NotBlank
     private String content;
 
     @Builder
-    public CreateTradeRequest(Long accountId, Long qrCodeId, int amount, String content) {
+    public CreateTradeRequest(Long accountId, String withdrawalAccountNumber, Long qrCodeId, String depositAccountNumber, int amount, String content) {
         this.accountId = accountId;
+        this.withdrawalAccountNumber = withdrawalAccountNumber;
         this.qrCodeId = qrCodeId;
+        this.depositAccountNumber = depositAccountNumber;
         this.amount = amount;
         this.content = content;
     }
--- server/src/main/java/com/shinhan/changyo/api/service/trade/dto/CreateTradeDto.java
+++ server/src/main/java/com/shinhan/changyo/api/service/trade/dto/CreateTradeDto.java
@@ -6,14 +6,18 @@ import lombok.Data;
 @Data
 public class CreateTradeDto {
     private Long accountId;
+    private String withdrawalAccountNumber;
     private Long qrCodeId;
+    private String depositAccountNumber;
     private int amount;
     private String content;
 
     @Builder
-    public CreateTradeDto(Long accountId, Long qrCodeId, int amount, String content) {
+    public CreateTradeDto(Long accountId, String withdrawalAccountNumber, Long qrCodeId, String depositAccountNumber, int amount, String content) {
         this.accountId = accountId;
+        this.withdrawalAccountNumber = withdrawalAccountNumber;
         this.qrCodeId = qrCodeId;
+        this.depositAccountNumber = depositAccountNumber;
         this.amount = amount;
         this.content = content;
     }
