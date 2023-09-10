47b0d7ce6f8d3c3cefe764f0d663cedfbe1940d1
longBright <longbright0804@gmail.com>
2023-09-10 17:32:58 +0900

    feat: 보증금 반환 서비스 스켈레톤 생성 - TradeService#returnDeposit

--- server/src/main/java/com/shinhan/changyo/api/service/trade/TradeService.java
+++ server/src/main/java/com/shinhan/changyo/api/service/trade/TradeService.java
@@ -1,11 +1,14 @@
 package com.shinhan.changyo.api.service.trade;
 
 import com.shinhan.changyo.api.service.trade.dto.CreateTradeDto;
+import com.shinhan.changyo.api.service.trade.dto.ReturnDepositDto;
 import com.shinhan.changyo.domain.trade.repository.TradeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 보증금 거래내역 서비스
@@ -27,6 +30,13 @@ public class TradeService {
      * @return 보증금 거래내역 식별키
      */
     public Long createTrade(CreateTradeDto dto) {
+        // 신한 잔액 조회 호출해서 예외처리 먼저
+        // 출금 계좌에서 돈빼고 거래내역 생성
+        // 신한 이체 API 호출
         return null;
+    }
+
+    public Boolean returnDeposit(List<ReturnDepositDto> dtos) {
+        return false;
     }
 }
