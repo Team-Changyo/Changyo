5366e043572a4d376c3dab538dd239702a2aef25
longBright <longbright0804@gmail.com>
2023-09-10 14:55:00 +0900

    docs: 보증금 정산관리 상세조회 API Docs 작성 - TradeControllerDocsTest#getDepositTradesDetail

--- server/src/test/java/com/shinhan/changyo/docs/trade/TradeControllerDocsTest.java
+++ server/src/test/java/com/shinhan/changyo/docs/trade/TradeControllerDocsTest.java
@@ -2,10 +2,7 @@ package com.shinhan.changyo.docs.trade;
 
 import com.shinhan.changyo.api.controller.trade.TradeController;
 import com.shinhan.changyo.api.controller.trade.request.CreateTradeRequest;
-import com.shinhan.changyo.api.controller.trade.response.DepositOverviewResponse;
-import com.shinhan.changyo.api.controller.trade.response.DepositResponse;
-import com.shinhan.changyo.api.controller.trade.response.WithdrawalDetailResponse;
-import com.shinhan.changyo.api.controller.trade.response.WithdrawalResponse;
+import com.shinhan.changyo.api.controller.trade.response.*;
 import com.shinhan.changyo.api.service.trade.TradeQueryService;
 import com.shinhan.changyo.api.service.trade.TradeService;
 import com.shinhan.changyo.api.service.trade.dto.CreateTradeDto;
@@ -18,13 +15,14 @@ import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -97,14 +95,14 @@ public class TradeControllerDocsTest extends RestDocsSupport {
                 .qrCodeTitle("럭셔리 글램핑 객실이용")
                 .depositName("전인혁")
                 .amount(20000)
-                .returnDate(TradeStatus.WAIT.getText())
+                .tradeDate(TradeStatus.WAIT.getText())
                 .build();
         WithdrawalDetailResponse doneDetail1 = WithdrawalDetailResponse.builder()
                 .tradeId(1L)
                 .qrCodeTitle("럭셔리 글램핑 2호점 객실이용")
                 .depositName("전인혁")
                 .amount(30000)
-                .returnDate("2023-03-08 19:30")
+                .tradeDate("2023-03-08 19:30")
                 .build();
         List<WithdrawalDetailResponse> waitDetails = List.of(waitDetail1);
         List<WithdrawalDetailResponse> doneDetails = List.of(doneDetail1);
@@ -149,7 +147,7 @@ public class TradeControllerDocsTest extends RestDocsSupport {
                                         .description("송금처"),
                                 fieldWithPath("data.waitWithdrawals[].amount").type(JsonFieldType.NUMBER)
                                         .description("송금 금액"),
-                                fieldWithPath("data.waitWithdrawals[].returnDate").type(JsonFieldType.STRING)
+                                fieldWithPath("data.waitWithdrawals[].tradeDate").type(JsonFieldType.STRING)
                                         .description("반환일시"),
                                 fieldWithPath("data.doneWithdrawals").type(JsonFieldType.ARRAY)
                                         .description("반환완료 거래내역리스트"),
@@ -161,7 +159,7 @@ public class TradeControllerDocsTest extends RestDocsSupport {
                                         .description("송금처"),
                                 fieldWithPath("data.doneWithdrawals[].amount").type(JsonFieldType.NUMBER)
                                         .description("송금 금액"),
-                                fieldWithPath("data.doneWithdrawals[].returnDate").type(JsonFieldType.STRING)
+                                fieldWithPath("data.doneWithdrawals[].tradeDate").type(JsonFieldType.STRING)
                                         .description("반환일시")
                         )
                 ));
@@ -228,6 +226,80 @@ public class TradeControllerDocsTest extends RestDocsSupport {
                                         .description("보증금 미반환 총액"),
                                 fieldWithPath("data.depositOverviews[].remainCount").type(JsonFieldType.NUMBER)
                                         .description("보증금 미반환 건수")
+                        )
+                ));
+    }
+
+    @DisplayName("보증금 정산관리 상세조회 API")
+    @Test
+    void getDepositTradesDetail() throws Exception {
+        String qrCodeId = "1";
+        String status = "반환 전";
+
+        DepositDetail detail2 = DepositDetail.builder()
+                .tradeId(1L)
+                .withdrawalName("임우택")
+                .tradeDate("2023-08-21 10:35")
+                .build();
+
+        DepositDetail detail1 = DepositDetail.builder()
+                .tradeId(2L)
+                .withdrawalName("홍진식")
+                .tradeDate("2023-08-24 14:30")
+                .build();
+        List<DepositDetail> details = List.of(detail1, detail2);
+
+        DepositDetailResponse response = DepositDetailResponse.builder()
+                .qrCodeTitle("럭셔리 글램핑 객실이용")
+                .amount(20000)
+                .remainTotal(40000)
+                .remainCount(2)
+                .depositDetails(details)
+                .build();
+
+        given(tradeQueryService.getDepositDetails(anyLong(), anyString()))
+                .willReturn(response);
+
+        mockMvc.perform(
+                        get("/trade/deposit/detail")
+                                .param("qrCodeId", qrCodeId)
+                                .param("status", status)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("search-deposit-details",
+                        preprocessResponse(prettyPrint()),
+                        requestParameters(
+                                parameterWithName("qrCodeId")
+                                        .description("보증금 정산관리 식별키"),
+                                parameterWithName("status")
+                                        .description("거래 상태")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답 데이터"),
+                                fieldWithPath("data.qrCodeTitle").type(JsonFieldType.STRING)
+                                        .description("보증금 정산관리 이름"),
+                                fieldWithPath("data.amount").type(JsonFieldType.NUMBER)
+                                        .description("입금 단위"),
+                                fieldWithPath("data.remainTotal").type(JsonFieldType.NUMBER)
+                                        .description("미반환 총액"),
+                                fieldWithPath("data.remainCount").type(JsonFieldType.NUMBER)
+                                        .description("미반환 건수"),
+                                fieldWithPath("data.depositDetails").type(JsonFieldType.ARRAY)
+                                        .description("보증금 정산관리 상세목록"),
+                                fieldWithPath("data.depositDetails[].tradeId").type(JsonFieldType.NUMBER)
+                                        .description("보증금 거래내역 식별키"),
+                                fieldWithPath("data.depositDetails[].withdrawalName").type(JsonFieldType.STRING)
+                                        .description("입금자명"),
+                                fieldWithPath("data.depositDetails[].tradeDate").type(JsonFieldType.STRING)
+                                        .description("입금일시")
                         )
                 ));
     }
