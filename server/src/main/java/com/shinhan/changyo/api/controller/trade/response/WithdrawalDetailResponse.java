71c0b60f4478fcdc3d9bb14209d43a37e6c6adc0
longBright <longbright0804@gmail.com>
2023-09-10 22:03:27 +0900

    fix: 보증금 송금관리 조회 API 수정 - TradeController#getWithdrawalTrades - WithdrawalDetailResponse.java - WithdrawalResponse.java

--- server/src/main/java/com/shinhan/changyo/api/controller/trade/response/WithdrawalDetailResponse.java
+++ server/src/main/java/com/shinhan/changyo/api/controller/trade/response/WithdrawalDetailResponse.java
@@ -1,23 +1,29 @@
 package com.shinhan.changyo.api.controller.trade.response;
 
+import com.shinhan.changyo.domain.trade.TradeStatus;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class WithdrawalDetailResponse {
 
     private Long tradeId;
     private String qrCodeTitle;
-    private String depositName;
-    private int amount;
+    private String memberName;
+    private int withdrawalAmount;
+    private TradeStatus status;
     private String tradeDate;
 
     @Builder
-    public WithdrawalDetailResponse(Long tradeId, String qrCodeTitle, String depositName, int amount, String tradeDate) {
+    public WithdrawalDetailResponse(Long tradeId, String qrCodeTitle, String memberName, int withdrawalAmount, TradeStatus status, LocalDateTime tradeDate) {
         this.tradeId = tradeId;
         this.qrCodeTitle = qrCodeTitle;
-        this.depositName = depositName;
-        this.amount = amount;
-        this.tradeDate = tradeDate;
+        this.memberName = memberName;
+        this.withdrawalAmount = withdrawalAmount;
+        this.status = status;
+        this.tradeDate = tradeDate.format(DateTimeFormatter.ofPattern("yyyyMMdd hhmm"));
     }
 }
--- server/src/main/java/com/shinhan/changyo/api/controller/trade/response/WithdrawalResponse.java
+++ server/src/main/java/com/shinhan/changyo/api/controller/trade/response/WithdrawalResponse.java
@@ -8,15 +8,15 @@ import java.util.List;
 @Data
 public class WithdrawalResponse {
 
-    private int remainCount;
-    private int finishCount;
+    private int waitCount;
+    private int doneCount;
     private List<WithdrawalDetailResponse> waitWithdrawals;
     private List<WithdrawalDetailResponse> doneWithdrawals;
 
     @Builder
-    public WithdrawalResponse(int remainCount, int finishCount, List<WithdrawalDetailResponse> waitWithdrawals, List<WithdrawalDetailResponse> doneWithdrawals) {
-        this.remainCount = remainCount;
-        this.finishCount = finishCount;
+    public WithdrawalResponse(int waitCount, int doneCount, List<WithdrawalDetailResponse> waitWithdrawals, List<WithdrawalDetailResponse> doneWithdrawals) {
+        this.waitCount = waitCount;
+        this.doneCount = doneCount;
         this.waitWithdrawals = waitWithdrawals;
         this.doneWithdrawals = doneWithdrawals;
     }
