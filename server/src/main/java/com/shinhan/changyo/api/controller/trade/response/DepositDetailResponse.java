f7dbc477074774ac48fca035777004f25638b6e5
longBright <longbright0804@gmail.com>
2023-09-11 01:29:32 +0900

    fix: 보증금 정산관리 상세 조회 API 수정 - TradeController#getDepositDetails - DepositDetailResponse

--- server/src/main/java/com/shinhan/changyo/api/controller/trade/response/DepositDetailResponse.java
+++ server/src/main/java/com/shinhan/changyo/api/controller/trade/response/DepositDetailResponse.java
@@ -9,16 +9,21 @@ import java.util.List;
 public class DepositDetailResponse {
     private String qrCodeTitle;
     private int amount;
-    private int remainTotal;
-    private int remainCount;
-    List<DepositDetail> depositDetails;
+    private int totalAmount;
+    private int waitCount;
+    private int doneCount;
+    List<DepositDetailDto> waitDetails;
+    List<DepositDetailDto> doneDetails;
 
     @Builder
-    public DepositDetailResponse(String qrCodeTitle, int amount, int remainTotal, int remainCount, List<DepositDetail> depositDetails) {
+    public DepositDetailResponse(String qrCodeTitle, int amount, int totalAmount, int waitCount, int doneCount, List<DepositDetailDto> waitDetails, List<DepositDetailDto> doneDetails) {
         this.qrCodeTitle = qrCodeTitle;
         this.amount = amount;
-        this.remainTotal = remainTotal;
-        this.remainCount = remainCount;
-        this.depositDetails = depositDetails;
+        this.totalAmount = totalAmount;
+        this.waitCount = waitCount;
+        this.doneCount = doneCount;
+        this.waitDetails = waitDetails;
+        this.doneDetails = doneDetails;
     }
+
 }
