ded92fa27b132e8886622d75386c3ca32fb875b9
longBright <longbright0804@gmail.com>
2023-09-10 19:34:00 +0900

    refactor: 보증금 송금 API 요청 객체 수정

--- server/src/main/java/com/shinhan/changyo/api/service/trade/dto/CreateTradeDto.java
+++ server/src/main/java/com/shinhan/changyo/api/service/trade/dto/CreateTradeDto.java
@@ -1,5 +1,9 @@
 package com.shinhan.changyo.api.service.trade.dto;
 
+import com.shinhan.changyo.domain.account.Account;
+import com.shinhan.changyo.domain.qrcode.QrCode;
+import com.shinhan.changyo.domain.trade.Trade;
+import com.shinhan.changyo.domain.trade.TradeStatus;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,19 +12,31 @@ public class CreateTradeDto {
     private Long accountId;
     private String withdrawalAccountNumber;
     private Long qrCodeId;
-    private String qrCodeTitle;
     private String depositAccountNumber;
     private int amount;
     private String content;
 
     @Builder
-    public CreateTradeDto(Long accountId, String withdrawalAccountNumber, Long qrCodeId, String qrCodeTitle, String depositAccountNumber, int amount, String content) {
+    public CreateTradeDto(Long accountId, String withdrawalAccountNumber, Long qrCodeId, String depositAccountNumber, int amount, String content) {
         this.accountId = accountId;
         this.withdrawalAccountNumber = withdrawalAccountNumber;
         this.qrCodeId = qrCodeId;
-        this.qrCodeTitle = qrCodeTitle;
         this.depositAccountNumber = depositAccountNumber;
         this.amount = amount;
         this.content = content;
+    }
+
+    public Trade toEntity(Account account, QrCode qrCode) {
+        return Trade.builder()
+                .summary("이체")
+                .withdrawalAmount(this.amount)
+                .depositAmount(this.amount)
+                .content(this.content)
+                .balance(account.getBalance())
+                .status(TradeStatus.WAIT)
+                .dealershipName("챙겨요")
+                .account(account)
+                .qrCode(qrCode)
+                .build();
     }
 }
