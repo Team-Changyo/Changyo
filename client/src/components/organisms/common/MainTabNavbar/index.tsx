7c71b87a15bdc11d218bb4cd2337893724d5401e
wjs_5025 <wjs_5025@naver.com>
2023-09-11 01:40:06 +0900

    refactor: navbar 범용성 개선(버튼 컴포넌트 영역 추가)

--- client/src/components/organisms/common/MainTabNavbar/index.tsx
+++ client/src/components/organisms/common/MainTabNavbar/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import NavbarLayout from 'layouts/common/NavbarLayout';
 import { MainTabNavbarContainer } from './style';
 
 /**
  * 탭 타이틀만 적혀있는 상단 네비게이션바
  */
-function MainTabNavbar({ tabName }: { tabName: string }) {
+function MainTabNavbar({ tabName, navBtn }: { tabName: string; navBtn?: ReactNode }) {
 	return (
 		<NavbarLayout>
 			<MainTabNavbarContainer>
 				<h1>{tabName}</h1>
+				<div className="nav-btn">{navBtn}</div>
 			</MainTabNavbarContainer>
 		</NavbarLayout>
 	);
