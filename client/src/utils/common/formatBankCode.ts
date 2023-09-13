527c1ea1b30b63f13edfd67d288bbc03e2e73af8
wjs_5025 <wjs_5025@naver.com>
2023-09-14 02:04:05 +0900

    refactor: 은행코드 변환 개선

--- client/src/utils/common/formatBankCode.ts
+++ client/src/utils/common/formatBankCode.ts
@@ -1,3 +1,5 @@
+import { BANKNAMES_BY_BANKCODES } from 'constants/bankCode';
+
 /**
  * number 타입 금액 -> locale에 따른 단위로 변환 (원, 달러 등)
  * @param amount 얼마?
@@ -5,9 +7,5 @@
  * @returns 변환된 금액
  */
 export const formatBankCode = (bankCode: string): string => {
-	const BANKNAMES: { [key: string]: string } = {
-		'088': '신한',
-	};
-
-	return BANKNAMES[bankCode];
+	return BANKNAMES_BY_BANKCODES.find((el) => el.key === bankCode)!.value;
 };
