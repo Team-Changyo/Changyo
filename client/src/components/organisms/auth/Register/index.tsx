d67a200ca4ab1acbf8589fabe266bab9a809d69c
wjs_5025 <wjs_5025@naver.com>
2023-09-13 22:16:34 +0900

    refactor: 버튼 컴포넌트 위치 이동

--- client/src/components/organisms/auth/Register/index.tsx
+++ client/src/components/organisms/auth/Register/index.tsx
@@ -1,5 +1,4 @@
-import React, { Dispatch, SetStateAction, useState } from 'react';
-import Button from 'components/organisms/common/Button';
+import React, { Dispatch, SetStateAction } from 'react';
 import { RegisterContainer } from './style';
 import RegisterForm from '../RegisterForm';
 
@@ -7,24 +6,9 @@ interface IRegisterProps {
 	setStep: Dispatch<SetStateAction<number>>;
 }
 function Register({ setStep }: IRegisterProps) {
-	const [isDone, setIsDone] = useState(false);
-
-	const handleClick = () => {
-		if (isDone) {
-			setStep(2);
-		} else {
-			// TODO : 토스트 메시지로 변경할 것.
-			console.log('모두 체크해라');
-		}
-	};
-
 	return (
 		<RegisterContainer>
-			<RegisterForm setIsDone={setIsDone} />
-			{/* 회원가입 완료 */}
-			<div className="next-btn">
-				<Button handleClick={handleClick} type={isDone ? 'Primary' : 'Normal'} text="회원가입 완료" />
-			</div>
+			<RegisterForm setStep={setStep} />
 		</RegisterContainer>
 	);
 }
