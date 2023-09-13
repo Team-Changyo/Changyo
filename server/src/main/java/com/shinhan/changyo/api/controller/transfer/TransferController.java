f1d0d51c2d8d9d4b765535da9d44dd6b1436136b
longBright <longbright0804@gmail.com>
2023-09-14 02:50:06 +0900

    feat: 보증금 송금 정보 조회 API 컨트롤러 스켈레톤 작성

---
+++ server/src/main/java/com/shinhan/changyo/api/controller/transfer/TransferController.java
@@ -0,0 +1,46 @@
+package com.shinhan.changyo.api.controller.transfer;
+
+import com.shinhan.changyo.api.ApiResponse;
+import com.shinhan.changyo.api.controller.transfer.response.TransferInfoResponse;
+import com.shinhan.changyo.api.service.transfer.TransferQueryService;
+import com.shinhan.changyo.security.SecurityUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 이체 관련 API 컨트롤러
+ *
+ * @author 최영환
+ */
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/transfer")
+public class TransferController {
+
+    private final TransferQueryService transferQueryService;
+
+    /**
+     * 보증금 송금 정보 조회 API
+     *
+     * @param qrCodeId QR 코드 식별키
+     * @return 보증금 이체 정보
+     */
+    @GetMapping
+    public ApiResponse<TransferInfoResponse> getTransferInfo(@RequestParam Long qrCodeId) {
+        log.debug("TransferController#getQrTransferInfo call");
+        log.debug("qrCodeId={}", qrCodeId);
+
+        String loginId = SecurityUtil.getCurrentLoginId();
+        log.debug("loginId={}", loginId);
+
+        TransferInfoResponse response = transferQueryService.getTransferInfo(qrCodeId, loginId);
+        log.debug("TransferResponse={}", response);
+
+        return ApiResponse.ok(response);
+    }
+}
