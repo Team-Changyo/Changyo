9a96a7b15b7931359053b63994dc6783430e3f65
wjs_5025 <wjs_5025@naver.com>
2023-09-14 02:02:14 +0900

    feat: 계좌 전체조회 api 연결

--- client/src/components/organisms/account/AccountList/index.tsx
+++ client/src/components/organisms/account/AccountList/index.tsx
@@ -1,35 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { IAccount } from 'types/account';
 import AccountListItem from '../AccountListItem';
 import { AccountListWrapper } from './style';
 
 interface AccountListProps {
-	accountList: [{ code: string }];
+	accountList: IAccount[];
 	selectedCode: string;
 }
 /**
  * 계좌 목록 (썸네일, 계좌별명, 잔액, 주계좌 여부 등)
  */
 function AccountList({ accountList, selectedCode }: AccountListProps) {
-	const [accounts, setAccounts] = useState([{ code: '000' }, { code: '088' }]);
-	// const [accounts, setAccounts] = useState(accountList);
+	const [accounts, setAccounts] = useState<IAccount[]>(accountList);
 
-	useState(() => {
+	useEffect(() => {
 		if (selectedCode === '000') {
-			// setAccounts(accountList);
+			setAccounts(accountList);
 		} else {
-			// TODO : API 나오면 .filter로 거를 것. (아래콘솔은 에러 방지)
-			// setAccounts(accountList);
-			console.log(accountList);
-			console.log(setAccounts);
+			setAccounts(accountList.filter((el) => el.bankCode === selectedCode));
 		}
-	});
+	}, [accountList, selectedCode]);
 
 	// TODO api 나오면 데이터 교체
 	return (
 		<AccountListWrapper>
-			{accounts.map((el) => {
-				return <AccountListItem key={el.code} isMainAccount={el.code === '0'} />;
-			})}
+			{accounts.length ? (
+				accounts.map((el) => {
+					return <AccountListItem key={el.accountId} isMainAccount={el.mainAccount} />;
+				})
+			) : (
+				<div>현재 등록된 계좌가 없습니다.</div>
+			)}
 		</AccountListWrapper>
 	);
 }
