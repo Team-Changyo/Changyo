cecbfda1549ed057af0e8a326114a08aeca0bdbd
longBright <longbright0804@gmail.com>
2023-09-15 01:54:15 +0900

    feat: 간편송금 이체정보 조회 서비스 구현

--- server/src/main/java/com/shinhan/changyo/api/service/transfer/TransferQueryService.java
+++ server/src/main/java/com/shinhan/changyo/api/service/transfer/TransferQueryService.java
@@ -1,13 +1,17 @@
 package com.shinhan.changyo.api.service.transfer;
 
-import com.shinhan.changyo.api.controller.transfer.response.ClientAccountResponse;
-import com.shinhan.changyo.api.controller.transfer.response.StoreAccountResponse;
-import com.shinhan.changyo.api.controller.transfer.response.TransferInfoResponse;
+import com.shinhan.changyo.api.controller.transfer.response.*;
+import com.shinhan.changyo.domain.qrcode.SimpleQrCode;
+import com.shinhan.changyo.domain.qrcode.repository.SimpleQrCodeRepository;
 import com.shinhan.changyo.domain.transfer.TransferQueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 
 /**
  * 이체정보 조회 서비스
@@ -21,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class TransferQueryService {
 
     private final TransferQueryRepository transferQueryRepository;
+    private final SimpleQrCodeRepository simpleQrCodeRepository;
 
     /**
      * 보증금 이체 정보 조회
@@ -31,7 +36,7 @@ public class TransferQueryService {
      */
     public TransferInfoResponse getTransferInfo(Long qrCodeId, String loginId) {
         ClientAccountResponse clientAccount = transferQueryRepository.getClientAccount(loginId);
-        StoreAccountResponse storeAccount = transferQueryRepository.getStoreAccount(qrCodeId);
+        StoreAccountResponse storeAccount = transferQueryRepository.getStoreAccountByQrCodeId(qrCodeId);
 
         return TransferInfoResponse.of(storeAccount, clientAccount);
     }
@@ -43,7 +48,29 @@ public class TransferQueryService {
      * @param loginId        현재 로그인 중인 회원 로그인 아이디
      * @return 간편송금 이체 정보
      */
-    public TransferInfoResponse getSimpleTransferInfo(Long simpleQrCodeId, String loginId) {
-        return null;
+    public SimpleTransferInfoResponse getSimpleTransferInfo(Long simpleQrCodeId, String loginId) {
+        SimpleQrCode simpleQrCode = simpleQrCodeRepository.findById(simpleQrCodeId)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 QR 코드입니다."));
+        Duration diff = Duration.between(simpleQrCode.getLastModifiedDate(), LocalDateTime.now());
+        log.debug("diff={}", diff.toMinutes());
+
+        if (isExpired(diff.toMinutes())) {
+            simpleQrCodeRepository.delete(simpleQrCode);
+            throw new IllegalArgumentException("유효시간이 만료되었습니다.");
+        }
+
+        ClientAccountResponse clientAccount = transferQueryRepository.getClientAccount(loginId);
+        SimpleStoreAccountResponse storeAccount = transferQueryRepository.getStoreAccountBySimpleQrCodeId(simpleQrCodeId);
+        return SimpleTransferInfoResponse.of(storeAccount, clientAccount);
+    }
+
+    /**
+     * 만료여부 확인
+     *
+     * @param minutes 현재시간과 최종수정시간 차이 (분)
+     * @return true: 3분이 지난 경우, false: 3분이 지나지 않은 경우
+     */
+    private boolean isExpired(long minutes) {
+        return minutes > 3;
     }
 }
