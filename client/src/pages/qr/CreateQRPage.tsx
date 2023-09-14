51fabfc3f808f9cb4664f9e8835abd19d370ef43
wjs_5025 <wjs_5025@naver.com>
2023-09-14 23:49:24 +0900

    feat: qr 생성 api 연결

--- client/src/pages/qr/CreateQRPage.tsx
+++ client/src/pages/qr/CreateQRPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ReactComponent as QR } from 'assets/icons/qr/qr-default-icon.svg';
 import CreateQRPageLayout from 'layouts/page/qr/CreateQRPageLayout';
 import PageLayout from 'layouts/common/PageLayout';
@@ -9,30 +9,46 @@ import Button from 'components/organisms/common/Button';
 import UnderLineInput from 'components/atoms/common/UnderLineInput';
 import AccountSelector from 'components/organisms/account/AccountSelector';
 import { IAccount } from 'types/account';
+import useAccountList from 'hooks/useAccountList';
+import { createQRApi } from 'utils/apis/qr';
+import { toast } from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 function CreateQRPage() {
-	const [remittanceType, setRemittanceType] = useState(0);
-
-	const accounts: IAccount[] = [
-		{
-			key: 0,
-			alias: '항상 가난한 내 신한',
-			bankCode: '088',
-			accountNumber: '12345615496',
-			accountHolder: '전인혁',
-		},
-	];
-
-	const [selectedAccount, setSelectedAccount] = useState<IAccount>(accounts[0]);
+	const accountList = useAccountList();
+	const [remittanceType, setRemittanceType] = useState(0); // 송금 타입 (단순 송금/보증금 송금)
+	const [moneyUnit, setMoneyUnit] = useState(0); // 보낼 금액
+	const [alias, setAlias] = useState(''); // 표시명
+	const [selectedAccount, setSelectedAccount] = useState<IAccount>(accountList[0]); // 현재 선택된 계좌
+	const navigate = useNavigate();
 
 	const tabTypes = [
 		{ idx: 0, title: '단순 송금', handleClick: () => setRemittanceType(0), selected: remittanceType },
 		{ idx: 1, title: '보증금 송금', handleClick: () => setRemittanceType(1), selected: remittanceType },
 	];
 
-	const createQRRequest = () => {
+	const createQRRequest = async () => {
+		try {
+			const body = {
+				accountId: accountList[0].accountId,
+				amount: moneyUnit,
+				title: alias,
+			};
+			const response = await createQRApi(body);
+			console.log(response);
+			if (response.status === 201) {
+				toast.success('송금 QR 생성이 완료되었습니다.');
+				navigate(`/qr/deposit/${response.data.data.qrCodeId}`);
+			}
+		} catch (error) {
+			console.error(error);
+		}
 		console.log(remittanceType);
 	};
+
+	useEffect(() => {
+		console.log(accountList);
+	}, [accountList]);
 
 	return (
 		<PageLayout>
@@ -42,13 +58,28 @@ function CreateQRPage() {
 				SelectRemittanceType={<SelectTabTypeList tabTypes={tabTypes} />}
 				AccountInfoTitle={<OptionTitleText text="입금받을 계좌" />}
 				SelectAccount={
-					<AccountSelector accounts={accounts} selected={selectedAccount!} setSelected={setSelectedAccount} />
+					<AccountSelector accounts={accountList} selected={selectedAccount} setSelected={setSelectedAccount} />
 				}
 				MoneyUnitTitle={<OptionTitleText text="얼마를 받을까요?" />}
-				InputMoneyUnit={<UnderLineInput placeholder="금액" unitText="원" width="150px" />}
+				InputMoneyUnit={
+					<UnderLineInput
+						placeholder="금액"
+						unitText="원"
+						width="150px"
+						value={moneyUnit}
+						setValue={setMoneyUnit}
+						type="number"
+					/>
+				}
 				CreateQRBtn={<Button handleClick={createQRRequest} text="송금 QR 만들기" type="Primary" icon={<QR />} />}
 				DisplayNameTitle={remittanceType ? <OptionTitleText text="요청 표시명을 입력하세요" /> : <div />}
-				InputDisplayName={remittanceType ? <UnderLineInput placeholder="표시명" width="240px" /> : <div />}
+				InputDisplayName={
+					remittanceType ? (
+						<UnderLineInput placeholder="표시명" width="240px" value={alias} setValue={setAlias} type="text" />
+					) : (
+						<div />
+					)
+				}
 			/>
 		</PageLayout>
 	);
