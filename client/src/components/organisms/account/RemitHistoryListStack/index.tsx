a66f5c37b4719062736b6fd042712357a1d7cf6e
wjs_5025 <wjs_5025@naver.com>
2023-09-15 03:24:44 +0900

    feat: 계좌 송금내역 조회

--- client/src/components/organisms/account/RemitHistoryListStack/index.tsx
+++ client/src/components/organisms/account/RemitHistoryListStack/index.tsx
@@ -1,31 +1,18 @@
 import React from 'react';
+import { IDetailInfo } from 'types/account';
 import { RemitHistoryListStackContianer } from './style';
 import HistoryList from '../RemitHistoryList';
 
-function RemitHistoryListStack() {
-	const history = [
-		{
-			date: '2023-09-04',
-			history: [
-				{ key: 0, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
-				{ key: 1, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
-			],
-		},
-		{
-			date: '2023-09-05',
-			history: [
-				{ key: 2, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
-				{ key: 3, title: '아이센스PC방', time: '23:23', price: -11000, balance: 200501 },
-			],
-		},
-	];
+function RemitHistoryListStack({ detailInfo }: { detailInfo: IDetailInfo }) {
+	console.log(detailInfo);
+	const dates = Object.keys(detailInfo?.allTradeResponses);
 
 	return (
 		<RemitHistoryListStackContianer>
-			{history.map((el) => (
-				<div key={el.date} className="history-list">
-					<div className="date">{el.date}</div>
-					<HistoryList histories={el.history} />
+			{dates.map((el: string) => (
+				<div key={el} className="history-list">
+					<div className="date">{el}</div>
+					<HistoryList histories={detailInfo.allTradeResponses[el]} />
 				</div>
 			))}
 		</RemitHistoryListStackContianer>
