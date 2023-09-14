f400d598f048b829bac25f665e744438876b218d
longBright <longbright0804@gmail.com>
2023-09-15 01:54:51 +0900

    feat: 간편송금 이체정보 조회 쿼리 구현

--- server/src/main/java/com/shinhan/changyo/domain/transfer/TransferQueryRepository.java
+++ server/src/main/java/com/shinhan/changyo/domain/transfer/TransferQueryRepository.java
@@ -3,6 +3,7 @@ package com.shinhan.changyo.domain.transfer;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shinhan.changyo.api.controller.transfer.response.ClientAccountResponse;
+import com.shinhan.changyo.api.controller.transfer.response.SimpleStoreAccountResponse;
 import com.shinhan.changyo.api.controller.transfer.response.StoreAccountResponse;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ import javax.persistence.EntityManager;
 import static com.shinhan.changyo.domain.account.QAccount.account;
 import static com.shinhan.changyo.domain.member.QMember.member;
 import static com.shinhan.changyo.domain.qrcode.QQrCode.qrCode;
+import static com.shinhan.changyo.domain.qrcode.QSimpleQrCode.simpleQrCode;
 
 /**
  * 이체 정보 쿼리 저장소
@@ -53,7 +55,7 @@ public class TransferQueryRepository {
      * @param qrCodeId QR 코드 식별키
      * @return 입금(점주) 계좌 정보
      */
-    public StoreAccountResponse getStoreAccount(Long qrCodeId) {
+    public StoreAccountResponse getStoreAccountByQrCodeId(Long qrCodeId) {
         return queryFactory
                 .select(Projections.constructor(StoreAccountResponse.class,
                         qrCode.qrCodeId,
@@ -68,6 +70,26 @@ public class TransferQueryRepository {
                 .join(qrCode.account, account)
                 .join(account.member, member)
                 .where(qrCode.qrCodeId.eq(qrCodeId))
+                .fetchOne();
+    }
+
+    /**
+     * 간편송금 이체정보 조회
+     *
+     * @param simpleQrCodeId 간편 송금 QR 코드 식별키
+     * @return 간편송금 이체정보
+     */
+    public SimpleStoreAccountResponse getStoreAccountBySimpleQrCodeId(Long simpleQrCodeId) {
+        return queryFactory
+                .select(Projections.constructor(SimpleStoreAccountResponse.class,
+                        simpleQrCode.id,
+                        simpleQrCode.memberName,
+                        simpleQrCode.amount,
+                        simpleQrCode.bankCode,
+                        simpleQrCode.accountNumber
+                ))
+                .from(simpleQrCode)
+                .where(simpleQrCode.id.eq(simpleQrCodeId))
                 .fetchOne();
     }
 }
