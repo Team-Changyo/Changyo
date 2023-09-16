9b7fc374ebb9d8702cfead8a6ab2ec42bf3b266e
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-17 01:48:10 +0900

    feat: 은행 필터 api 연결

--- client/src/pages/account/AccountPage.tsx
+++ client/src/pages/account/AccountPage.tsx
@@ -16,12 +16,21 @@ function AccountPage() {
 	const [selectedCode, setSelectedCode] = useState('000'); // 은행구분
 	const [accountList, setAccountList] = useState<IAccount[]>([]);
 	const [totalMoney, setTotalMoney] = useState(0);
+	const [bankList, setBankList] = useState<string[]>([]);
 	const navigate = useNavigate();
 
 	const fetchAccountData = async () => {
 		try {
 			const response = await findAllAccountApi();
+
 			if (response.status === 200) {
+				const arr: string[] = [];
+				response.data.data?.accountDetailResponses.forEach((el: IAccount) => {
+					arr.push(el?.bankCode);
+				});
+
+				const set = new Set(arr);
+				setBankList([...set]);
 				setAccountList(response.data.data.accountDetailResponses);
 				setTotalMoney(response.data.data.accountTotalBalance);
 			}
@@ -47,7 +56,7 @@ function AccountPage() {
 				}
 				AccountSummary={<AccountTotalSummary accountCnt={accountList.length} totalMoney={totalMoney} />}
 				AccountFilterList={
-					<AccountFilterList bankCodes={['088']} selectedCode={selectedCode} setSelectedCode={setSelectedCode} />
+					<AccountFilterList bankCodes={bankList} selectedCode={selectedCode} setSelectedCode={setSelectedCode} />
 				}
 				AccountList={<AccountList accountList={accountList} selectedCode={selectedCode} />}
 			/>
