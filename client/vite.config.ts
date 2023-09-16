6f372848781aea92bcd3e5210340eba0d29694dc
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-16 16:01:30 +0900

    build: base url 수정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
+	base: '/',
 	plugins: [react(), eslint(), tsconfigPaths(), svgr(), mkcert()],
 	resolve: {
 		extensions: ['.js', '.ts', '.tsx', '.jsx'],
