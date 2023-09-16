b45913469bdca2d2733269965e15a580fecf6b30
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-16 14:50:00 +0900

    feat: 더미데이터 -> 실 데이터로 전환

--- client/src/components/organisms/remittance/ToAccountInfo/index.tsx
+++ client/src/components/organisms/remittance/ToAccountInfo/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import AccountSelectListItem from 'components/organisms/account/AccountSelectListItem';
-import { IAccount } from 'types/account';
+import { IAccount, IStoreAccount } from 'types/account';
 import { ToAccountInfoContainer } from './style';
 
-function ToAccountInfo() {
+function ToAccountInfo({ storeAccount }: { storeAccount: IStoreAccount | undefined }) {
 	const account: IAccount = {
-		key: 1,
-		alias: '항상 가난한 내 신한',
-		accountNumber: '1234567895',
-		bankCode: '088',
-		accountHolder: '전인혁',
+		accountId: 0,
+		balance: 0,
+		mainAccount: true,
+		accountNumber: storeAccount?.accountNumber ?? '',
+		bankCode: storeAccount?.bankCode ?? '',
+		title: storeAccount?.memberName ?? '',
 	};
 	return (
 		<ToAccountInfoContainer>
 			<AccountSelectListItem account={account} onlyView />
 			<p>
-				예금주 <span className="account-holder-name">{account.accountHolder}</span>
+				예금주 <span className="account-holder-name">{account.title}</span>
 			</p>
 		</ToAccountInfoContainer>
 	);
