5d427ae7d051b1c9dac55c5081835a0b36381502
wjs_5025@naver.com <wjs_5025@naver.com>
2023-09-17 01:38:22 +0900

    design: tabbar 높이 키우기

--- client/src/components/organisms/common/Tabbar/style.ts
+++ client/src/components/organisms/common/Tabbar/style.ts
@@ -12,7 +12,7 @@ export const TabContainer = styled.nav`
 	display: flex;
 	width: 100%;
 	max-width: 500px;
-	height: 56px;
+	height: 80px;
 	margin: 0 auto;
 `;
 
@@ -27,7 +27,7 @@ export const TabBackground = styled.div`
 
 	#box {
 		width: 40px;
-		height: 40px;
+		height: 64px;
 		background-color: var(--white-color);
 		z-index: 10;
 	}
@@ -37,7 +37,7 @@ export const TabBackground = styled.div`
 		display: inline-block;
 		background: var(--white-color);
 		width: 50%;
-		height: 56px;
+		height: 80px;
 		box-shadow: 0 0 0.4rem 0 rgba(0, 0, 0, 0.16);
 		border-radius: 0 2rem 0 0;
 	}
@@ -47,7 +47,7 @@ export const TabBackground = styled.div`
 		display: inline-block;
 		background: var(--white-color);
 		width: 50%;
-		height: 56px;
+		height: 80px;
 		box-shadow: 0 0 0.4rem 0 rgba(0, 0, 0, 0.16);
 		border-radius: 2rem 0 0 0;
 	}
