6a272bbda2a422c22c5e8e4d6b7bd5abfe677a96
Chaos0103 <lyt1228@naver.com>
2023-09-16 17:18:26 +0900

    feat: 레디스 적용

--- server/src/main/java/com/shinhan/changyo/api/service/auth/AuthenticationService.java
+++ server/src/main/java/com/shinhan/changyo/api/service/auth/AuthenticationService.java
@@ -4,66 +4,57 @@ import com.shinhan.changyo.client.request.OneTransferRequest;
 import com.shinhan.changyo.client.response.OneTransferResponse;
 import com.shinhan.changyo.client.ShinHanApiClient;
 import com.shinhan.changyo.domain.account.repository.AccountQueryRepository;
-import com.shinhan.changyo.domain.authentication.Authentication;
-import com.shinhan.changyo.domain.authentication.repository.AuthenticationRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
-@RequiredArgsConstructor
 @Service
 public class AuthenticationService {
 
-    private final AuthenticationRepository authenticationRepository;
     private final AccountQueryRepository accountQueryRepository;
     private final ShinHanApiClient shinHanApiClient;
 
+    private final RedisTemplate<String, String> redisTemplate;
+    private final ValueOperations<String, String> valueOperations;
+
+    public AuthenticationService(AccountQueryRepository accountQueryRepository, ShinHanApiClient shinHanApiClient, RedisTemplate<String, String> redisTemplate) {
+        this.accountQueryRepository = accountQueryRepository;
+        this.shinHanApiClient = shinHanApiClient;
+        this.redisTemplate = redisTemplate;
+        this.valueOperations = redisTemplate.opsForValue();
+    }
+
     public String generateAuthenticationNumber(String bankCode, String accountNumber) {
         String randomNumber = generateRandomNumber();
+
         OneTransferRequest request = OneTransferRequest.builder()
             .bankCode(bankCode)
             .accountNumber(accountNumber)
             .memo(randomNumber)
             .build();
-
         OneTransferResponse response = shinHanApiClient.oneTransfer(request).getData();
 
-        Optional<Authentication> findAuthentication = authenticationRepository.findById(accountNumber);
-        if (findAuthentication.isEmpty()) {
-            Authentication authentication = Authentication.builder()
-                .accountNumber(accountNumber)
-                .authenticationNumber(randomNumber)
-                .build();
-            authenticationRepository.save(authentication);
-        } else {
-            Authentication authentication = findAuthentication.get();
-            authentication.editNumber(randomNumber);
-        }
+        valueOperations.set(accountNumber, randomNumber);
+
+        Boolean expire = redisTemplate.expire(accountNumber, 3, TimeUnit.MINUTES);
 
         return randomNumber;
     }
 
     public void checkAuthenticationNumber(String accountNumber, String authenticationNumber) {
-        Optional<Authentication> findAuthentication = authenticationRepository.findById(accountNumber);
-        if (findAuthentication.isEmpty()) {
-            throw new NoSuchElementException("재인증 요청 해주세요.");
-        }
-
-        Authentication authentication = findAuthentication.get();
-
-        if (authentication.getLastModifiedDate().plusMinutes(3).isBefore(LocalDateTime.now())) {
+        String number = valueOperations.get(accountNumber);
+        if (number == null) {
             throw new IllegalArgumentException("유효시간이 만료되었습니다.");
         }
 
-        if (!authentication.getAuthenticationNumber().equals(authenticationNumber)) {
+        if (!number.equals(authenticationNumber)) {
             throw new IllegalArgumentException("인증번호가 일치하지 않습니다.");
         }
 
-        authenticationRepository.deleteById(accountNumber);
+        redisTemplate.expire(accountNumber, 0, TimeUnit.SECONDS);
 
         boolean result = accountQueryRepository.checkIsExistByAccountNumber(accountNumber);
         if (result) {
